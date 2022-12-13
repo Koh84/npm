3ca17b74d7b52171394e25d6c032a2460b28ab6d
Kelvin Koh <kelvin.koh@prometheanworld.com>
2022-12-13 15:05:00 +0000

    added websocket connect, send and close

--- index.js
+++ index.js
@@ -2,4 +2,45 @@ function isWds(string) {
     return string === 'WDS'
 }
 
-module.exports = isWds
+let ws = null;
+let i = 0;
+
+const handleSocketConnect = async () => {
+    console.log('handleSocketConnect');
+    let serverAddress = 'ws:' + '127.0.0.1' + ':' + '3030';
+    ws = new WebSocket(serverAddress);
+    ws.onmessage = (message) => {
+        console.log('WebSocket: Message recieved: ' + message.data);
+    };
+    ws.onerror = (event) => {
+        console.log('Websocket: Error ' + event);
+    };
+    ws.onopen = () => {
+        console.log('Websocket: Connected to Server');
+    };
+    ws.onclose = () => {
+        console.log('WebSocket: Closing');
+    };
+    console.log('WebSocket: ready ');
+}
+
+const handleSocketSend = async () => {
+    if (ws !== null) {
+        ws.send(`This is a test message ${i++}`);
+        console.log('handleSocketSend: message sent ');
+    }
+}
+
+const handleSocketClose = async () => {
+    if (ws !== null) {
+        ws.close();
+        console.log('handleSocketClose');
+    }
+}
+
+module.exports = {
+    isWds,
+    handleSocketConnect,
+    handleSocketSend,
+    handleSocketClose
+}
